78a3803b6ce5cc97c4527350d67c1de98e13b30a
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2012-06-02 21:02:59 +0200

    rename X -> ~X

---
+++ Programowanie/~X/src/main/java/sddfg.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+/**
+ * User: Maciej Poleski
+ * Date: 20.03.12
+ * Time: 22:27
+ */
+public class sddfg {
+    private JTextPane gffhggjjghTextPane;
+    private JSlider slider1;
+    
+    int ghf()
+    {
+        org.eclipse.swt.browser.Browser
+    }
+}
